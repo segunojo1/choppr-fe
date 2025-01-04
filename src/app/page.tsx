41aadec6b2d51465ac981d9d612a8854b351999f
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2025-01-04 19:42:12 +0100

    ++ Why-Us component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Footer from "@/components/Footer";
+import WhyUs from "@/components/WhyUsSection";
 
 export default function Home() {
   return (
     <div className="">
       {/* import landing page components here, follow semantics */}
+      <WhyUs />
       <Footer />
     </div>
   );
