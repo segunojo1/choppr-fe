f42714cf9f9cc7cd4ead1ce2c178cfcf2cf58666
segunojo1 <olusegunkd2017@gmail.com>
2025-01-06 16:41:12 +0100

    feat: implement shorten url add table structure

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,14 @@
 import { Hero } from "@/components/dashboard/Hero";
+import LinkTable from "@/components/dashboard/link-table";
 import { Navbar } from "@/components/dashboard/Navbar";
 
 export default function Dashboard() {
     return (
-        <div className="bg-[#0B101B] dashboard h-screen bg-center">
+        <div className="bg-[#0B101B] dashboard min-h-screen bg-center">
             <Navbar />
             <main>
                 <Hero />
+                <LinkTable />
             </main>
         </div>
     );
