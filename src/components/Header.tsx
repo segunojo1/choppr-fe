ab888ce6297ff8dab2e20ac2aebddca883d0cc45
Thoobi <sydneydaniel76@gmail.com>
2025-01-07 18:22:41 +0100

    another commit to the header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
-import { CiCalendar } from "react-icons/ci";
+import { HiOutlineCalendar } from "react-icons/hi2";
 import { IoLinkOutline } from "react-icons/io5";
 import { IoIosStar } from "react-icons/io";
+import qrcode from '../../public/qrCode.png'
+import Image from "next/image";
 
 const Header = () => {
   return (
     <div className="bg-white text-black w-full">
       <div className="max-w-[85%] mx-auto">
-        <div className="mt-[50px] flex flex-row gap-10">
+        <div className="mt-[50px] flex flex-row gap-20">
           <div className="flex flex-col gap-[40px]">
             <span className="p-3 text-[#8039DF] font-medium align-middle bg-[#D6D6D6] rounded-3xl text-center w-[28rem]">Let&apos;s make links accessible to all with simply one click. 👈</span>
             <h1 className="text-7xl w-[46rem] font-bold uppercase">
@@ -22,7 +24,7 @@ const Header = () => {
             </div>
             <div className="flex flex-row gap-3">
               <div>
-                images
+                <Image src={""} alt="" />
               </div>
               <div className="flex flex-col items-start w-40">
                 <div className="flex flex-row gap-3 justify-center items-center">
@@ -35,39 +37,38 @@ const Header = () => {
                   </ul>
                   <h5 className="text-base font-bold">4.0</h5>
                 </div>
-
                 <h5 className="text-base font-normal text-[#7a7a7a]">from 500+ reviews</h5>
               </div>
             </div>
           </div>
 
           <div>
-            <div>
-              <span>
-                <h5>QR CODE</h5>
-                <button>Download PNG</button>
+            <div className="border-[2px] border[#D6D6D6] w-[460px] px-5 py-6 rounded-2xl flex flex-col gap-5">
+              <span className="flex flex-row justify-between items-center">
+                <h5 className="font-bold text-base">QR CODE</h5>
+                <button className="bg-[#8039DF] h-[43px] w-[150px] text-white rounded-lg text-sm font-medium">Download PNG</button>
               </span>
-              <div>
+              <div className="flex flex-row justify-between items-center">
                 <div>
-                  Image
+                  <Image src={qrcode} alt="this is a qrcode" width={110} height={120} />
                 </div>
                 <div>
-                  <ul>
-                    <li>
-                      <IoLinkOutline />
-                      <h5>https://chopper-be/Mailtumator</h5>
+                  <ul className="flex flex-col gap-2">
+                    <li className="flex flex-row gap-5 items-center">
+                      <IoLinkOutline size={25} className="text-[#8039DF]" />
+                      <h5 className="text-base text-black font-medium">https://chopper-be/mailtumator</h5>
                     </li>
-                    <li>
-                      <CiCalendar />
-                      <h5>22 December 2024</h5>
+                    <li className="flex flex-row gap-5 items-center">
+                      <HiOutlineCalendar size={25} className="text-[#8039DF]" />
+                      <h5 className="text-base text-black font-medium">22 December 2024</h5>
                     </li>
                   </ul>
                 </div>
               </div>
             </div>
-            <span>
-              image
-            </span>
+            <div className="h-12 w-12 bg-[#8039DF]">
+              <IoLinkOutline size={50} />
+            </div>
             <div>
               <h5 className="uppercase">Customize your link</h5>
             </div>
